8a7d5739cb60beedf8f8dc2414807e3254f1b980
Andrien Wiandyano <arnnied03@gmail.com>
2021-08-29 14:33:47 +0700

    add slight responsiveness and change input type

--- js/main.js
+++ js/main.js
@@ -10,34 +10,75 @@ function string_generator(length) {
   return result;
 }
 
+function io_storage(data, key) {
+  let storage = window.localStorage
+  const id = string_generator(10)
+
+  if(storage.getItem(key) != null) {
+    stored = JSON.parse(storage.getItem(key))
+    stored[id] = data
+
+    storage.setItem(key, JSON.stringify(stored))
+  } else {
+    let to_be_stored = {}
+    to_be_stored[id] = data
+
+    storage.setItem(key, JSON.stringify(to_be_stored))
+  }
+}
+
 function segitiga_hitung() {
     let tinggi = document.getElementById("segitiga-tinggi").value
     let alas = document.getElementById("segitiga-alas").value
 
-    let luas = parseInt(alas) * parseInt(tinggi) / 2
-    let keliling = parseInt(alas) * 3
+    let luas = parseFloat(alas) * parseFloat(tinggi) / 2
+    let keliling = parseFloat(alas) * 3
     
     document.getElementById("segitiga-luas").value = luas
     document.getElementById("segitiga-keliling").value = keliling
+
+    const data = {
+      time: Date.now(),
+      tinggi: tinggi,
+      alas: alas,
+      luas: luas,
+      keliling: keliling
+    }
+    io_storage(data, "segitiga")
 }
 
 function persegi_hitung() {
     let sisi = document.getElementById("persegi-sisi").value
 
-    let luas = parseInt(sisi)**2
-    let keliling = parseInt(sisi) * 4
+    let luas = parseFloat(sisi)**2
+    let keliling = parseFloat(sisi) * 4
 
     document.getElementById("persegi-luas").value = luas
     document.getElementById("persegi-keliling").value = keliling
+
+    const data = {
+      time: Date.now(),
+      sisi: sisi,
+      luas: luas,
+      keliling: keliling
+    }
+    io_storage(data, "persegi")
 }
 
 function lingkaran_hitung() {
     let jari_jari = document.getElementById("lingkaran-jari-jari").value
 
-    let luas = Math.PI * parseInt(jari_jari)**2
-    let keliling = Math.PI * 2 * parseInt(jari_jari)
+    let luas = Math.PI * parseFloat(jari_jari)**2
+    let keliling = Math.PI * 2 * parseFloat(jari_jari)
 
     document.getElementById("lingkaran-luas").value = luas
     document.getElementById("lingkaran-keliling").value = keliling
 
+    const data = {
+      time: Date.now(),
+      jari_jari: jari_jari,
+      luas: luas,
+      keliling: keliling 
+    }
+    io_storage(data, "lingkaran")
 }
